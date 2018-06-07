12c32f9873abb3baaa16bcaf470d80e0b36c9534
Andrey Gaisinskii <andygaisinskii@gmail.com>
2018-06-07 17:00:02 +0300

    added words API

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,37 @@
+// Getting APIs
 var giphyAPI = 'https://api.giphy.com/v1/gifs/random?api_key=rmvxuUnCdxyv6ZBJ3XHXMlP7bNmZlnnZ&tag=&rating=G';
-var randomGif = document.getElementById('random-gif');
+var wordAPI = 'https://api.whatdoestrumpthink.com/api/v1/quotes';
+// Getting elements from HTML
+var randomGif = document.getElementById('random-gif'); 
+var randomWord = document.getElementById('heading');
 
-
-var request = new XMLHttpRequest();
-request.open('GET', giphyAPI);
-request.responseType = 'json';
-request.send();
-
-request.onload = () => {
-  var giphyImg = request.response;
-  var giphyUrl = giphyImg.data.images.original.url;
-  var myImg = document.createElement('IMG');
-  myImg.setAttribute('src', giphyUrl);
-  randomGif.appendChild(myImg);
+gif = () => {
+  let request = new XMLHttpRequest();
+  request.open('GET', giphyAPI);
+  request.responseType = 'json';
+  request.send();
+  
+  request.onload = () => {
+    let giphyImg = request.response;
+    let giphyUrl = giphyImg.data.images.original.url;
+    let myImg = document.createElement('IMG');
+    myImg.setAttribute('src', giphyUrl);
+    randomGif.appendChild(myImg);
+  }
 }
+
+word = () => {
+  var request = new XMLHttpRequest();
+  request.open('GET', wordAPI);
+  request.responseType = 'json';
+  request.send();
+  
+  request.onload = () => {
+    var wordArray = request.response;
+    var wordCut = wordArray.messages.personalized[Math.floor(Math.random() * 500)];
+    randomWord.innerText = wordCut;
+  }
+}
+
+gif();
+word();
